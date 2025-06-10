0e850aa8057d3a15f9e5bdbac8b1c29c272369d0
Vera Gonzalez <me@vera.lgbt>
2025-06-10 00:36:26 -0400

    enum iteration

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,21 @@
 // The dioxus prelude contains a ton of common items used in dioxus apps. It's a good idea to import wherever you
 // need dioxus
 use dioxus::prelude::*;
-
-use views::{Blog, Home, Navbar};
+use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumIter};
 
 /// Define a components module that contains all shared components for our app.
 mod components;
 /// Define a views module that contains the UI for all Layouts and Routes for our app.
 mod views;
 
-/// The Route enum is used to define the structure of internal routes in our app. All route enums need to derive
-/// the [`Routable`] trait, which provides the necessary methods for the router to work.
-///
-/// Each variant represents a different URL pattern that can be matched by the router. If that pattern is matched,
-/// the components for that route will be rendered.
-#[derive(Debug, Clone, Routable, PartialEq)]
-#[rustfmt::skip]
-enum Route {
-    // The layout attribute defines a wrapper for all routes under the layout. Layouts are great for wrapping
-    // many routes with a common UI like a navbar.
-    #[layout(Navbar)]
-        // The route attribute defines the URL pattern that a specific route matches. If that pattern matches the URL,
-        // the component for that route will be rendered. The component name that is rendered defaults to the variant name.
-        #[route("/")]
-        Home {},
-        // The route attribute can include dynamic parameters that implement [`std::str::FromStr`] and [`std::fmt::Display`] with the `:` syntax.
-        // In this case, id will match any integer like `/blog/123` or `/blog/-456`.
-        #[route("/blog/:id")]
-        // Fields of the route variant will be passed to the component as props. In this case, the blog component must accept
-        // an `id` prop of type `i32`.
-        Blog { id: i32 },
+#[derive(Debug, Clone, EnumIter, PartialEq, Display)]
+enum Platonic {
+    Tetrahedron,
+    Hexahedron,
+    Octahedron,
+    Dodecahedron,
+    Icosahedron,
 }
 
 // We can import assets in dioxus with the `asset!` macro. This macro takes a path to an asset relative to the crate root.
@@ -70,17 +56,13 @@ fn App() -> Element {
                     div { class: "item has-sub",
                         "Recent"
                         div { class: "submenu",
-                            for i in 0..10 {
-                                div { class: "item", "file_{i}.doc" }
+                            for preset in Platonic::iter() {
+                                div { class: "item", "file_{preset}.doc" }
                             }
                         }
                     }
                 }
             }
         }
-
-        // The router component renders the route enum we defined above. It will handle synchronization of the URL and render
-        // the layouts and components for the active route.
-        Router::<Route> {}
     }
 }
